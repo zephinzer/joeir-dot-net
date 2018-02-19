8aaf7040d77d71ab1d55c0baf9880f9313f848be
zephinzer <dev@joeir.net>
2018-02-19 19:18:14 +0800

    updated to support persistent content

--- blog/code/.create-deployment.sh
+++ blog/code/.create-deployment.sh
@@ -6,10 +6,17 @@ DB_USERNAME_BASE64=`cat ${CURR_DIR}/.config/.DB_USERNAME | base64`;
 DB_PASSWORD_BASE64=`cat ${CURR_DIR}/.config/.DB_PASSWORD | base64`;
 DOMAIN=`cat ${CURR_DIR}/.config/.DOMAIN`;
 DOMAIN_ESCAPED="$(printf -- "${DOMAIN}" | sed -e "s|[^a-z0-9]|\-|g")";
+GCP_PERSISTENT_DISK_NAME=`cat ${CURR_DIR}/.config/.GCP_PERSISTENT_DISK_NAME`;
+GCP_PERSISTENT_DISK_SIZE=`cat ${CURR_DIR}/.config/.GCP_PERSISTENT_DISK_SIZE`;
 SUBDOMAIN=`cat ${CURR_DIR}/.config/.SUBDOMAIN | sed -e "s|[^a-z0-9\.]|\-|g"`;
 SUBDOMAIN_ESCAPED="$(printf -- "${SUBDOMAIN}" | sed -e "s|[^a-z0-9]|\-|g")";
 CLOUDSQL_CREDENTIALS_BASE64=`cat ${CURR_DIR}/.config/.CLOUDSQL_CREDENTIALS.json | base64`;
 CLOUDSQL_INSTANCE_CONNECTION_NAME=`cat ${CURR_DIR}/.config/.CLOUDSQL_INSTANCE_CONNECTION_NAME`;
+GHOST_PRODUCTION_CONFIG_JSON=`cat ${CURR_DIR}/.config/.GHOST_PRODUCTION_CONFIG.json`;
+GHOST_PRODUCTION_CONFIG='';
+for LINE in ${GHOST_PRODUCTION_CONFIG_JSON}; do
+  GHOST_PRODUCTION_CONFIG="${GHOST_PRODUCTION_CONFIG}${LINE}";
+done;
 CURRENT_TIMESTAMP=`date +'%Y%m%d-%H%M%S'`;
 PATH_TO_DEPLOYMENT="${CURR_DIR}/.deployments/${CURRENT_TIMESTAMP}/";
 mkdir -p "${PATH_TO_DEPLOYMENT}";
@@ -19,6 +26,10 @@ cat ${CURR_DIR}/config-map.yaml \
   | sed -e "s|\${DB_HOST}|${DB_HOST}|g" \
   | sed -e "s|\${DB_NAME}|${DB_NAME}|g" \
   >> ${PATH_TO_DEPLOYMENT}/config-map.yaml;
+cat ${CURR_DIR}/config-map.ghost-config.yaml \
+  | egrep "^[^\#]" \
+  | sed -e "s|\${GHOST_PRODUCTION_CONFIG}|${GHOST_PRODUCTION_CONFIG}|g" \
+  >> ${PATH_TO_DEPLOYMENT}/config-map.ghost-config.yaml;
 cat ${CURR_DIR}/secret.db.yaml \
   | egrep "^[^\#]" \
   | sed -e "s|\${DB_USERNAME_BASE64}|${DB_USERNAME_BASE64}|g" \
@@ -35,6 +46,15 @@ cat ${CURR_DIR}/secret.cloudsql.yaml \
   | egrep "^[^\#]" \
   | sed -e "s|\${CLOUDSQL_CREDENTIALS_BASE64}|${CLOUDSQL_CREDENTIALS_BASE64}|g" \
   >> ${PATH_TO_DEPLOYMENT}/secret.cloudsql.yaml;
+cat ${CURR_DIR}/persistent-volume.yaml \
+  | egrep "^[^\#]" \
+  | sed -e "s|\${GCP_PERSISTENT_DISK_NAME}|${GCP_PERSISTENT_DISK_NAME}|g" \
+  | sed -e "s|\${GCP_PERSISTENT_DISK_SIZE}|${GCP_PERSISTENT_DISK_SIZE}|g" \
+  >> ${PATH_TO_DEPLOYMENT}/persistent-volume.yaml;
+cat ${CURR_DIR}/persistent-volume-claim.yaml \
+  | egrep "^[^\#]" \
+  | sed -e "s|\${GCP_PERSISTENT_DISK_SIZE}|${GCP_PERSISTENT_DISK_SIZE}|g" \
+  >> ${PATH_TO_DEPLOYMENT}/persistent-volume-claim.yaml;
 cat ${CURR_DIR}/deployment.yaml \
   | egrep "^[^\#]" \
   | sed -e "s|\${CLOUDSQL_INSTANCE_CONNECTION_NAME}|${CLOUDSQL_INSTANCE_CONNECTION_NAME}|g" \
